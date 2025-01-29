aaaa16da6f0e2667e9ece78652c9146328a7089c
stackoverflow <44741044+whileloop-png@users.noreply.github.com>
2025-01-28 23:10:59 -0800

    Update kingvon.lua

--- kingvon.lua
+++ kingvon.lua
@@ -1,32 +1,31 @@
-local player_gui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+if not writefile then             warn(getexecutorname() .. " doesnt support writefile")
+elseif not getcustomasset then    warn(getexecutorname() .. " doesnt support getcustomasset")
+elseif not delfile then           warn(getexecutorname() .. " doesnt support delfile") end
+local plrgui                 = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+local vidgui                 = Instance.new("ScreenGui") vidgui.Parent = plrgui
+local vidframe               = Instance.new("VideoFrame")
+vidframe.Name                = "video_frame"
+vidframe.Size                = UDim2.new(1, 0, 1.1, 0)
+vidframe.Position            = UDim2.new(0, 0, -0.05, 0)
+vidframe.BackgroundColor3    = Color3.new(0, 0, 0)
+vidframe.Video               = ""
+vidframe.Parent              = vidgui
+local response               = nil
 
-local video_gui = Instance.new("ScreenGui")
-video_gui.Name = "video_gui"
-video_gui.Parent = player_gui
-
-local video_frame = Instance.new("VideoFrame")
-video_frame.Name = "video_frame"
-video_frame.Size = UDim2.new(1, 0, 1.1, 0)
-video_frame.Position = UDim2.new(0, 0, -0.05, 0)
-video_frame.BackgroundColor3 = Color3.new(0, 0, 0)
-video_frame.Video = ""
-video_frame.Parent = video_gui
-
-local response = request({
-    Url = "https://raw.githubusercontent.com/whileloop-png/proj/main/videoplayback%20(1).mp4",
-    Method = "GET"
-})
+    response                 = request({
+    Url                      = "https://raw.githubusercontent.com/whileloop-png/proj/main/videoplayback%20(1).mp4",
+    Method                   = "GET"})
 
 if response and response.Body then
-    local file_name = "videoplayback.mp4"
+    local file_name          = "videoplayback.mp4"
     writefile(file_name, response.Body)
-    local custom_asset = getcustomasset(file_name)
-    video_frame.Video = custom_asset
-    video_frame:Play()
-    video_frame.Ended:Connect(function()
+    local custom_asset       = getcustomasset(file_name)
+    vidframe.Video           = custom_asset
+    vidframe:Play()
+    vidframe.Ended:Connect(function()
         delfile(file_name)
         video_gui:Destroy()
     end)
 else
-    video_gui:Destroy()
+    vidgui:Destroy()
 end
